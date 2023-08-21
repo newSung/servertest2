b7aabd65d02e8f3f1a369cf19c7d9dd582d5fc30
newSung <gho0612@gmail.com>
2023-08-21 19:14:38 +0900

    x

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 
-app.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // 특정 도메인 허용
-});
 
-app.use(cors({
-    origin: 'https://gregarious-meerkat-c611cb.netlify.app',
-}));
+
+app.use(cors());
 
 app.get('/', (req, res) => { res.send('hellossssssssssssssss world') })
 
